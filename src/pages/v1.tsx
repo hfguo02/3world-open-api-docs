4dea77e3fc276f53bed278d8fccd4494caefaede
pancho <hfguo02@aliyun.com>
2026-08-26 19:41:45 +0800

    fix: preserve Pages base path in redirects

--- src/pages/v1.tsx
+++ src/pages/v1.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function LegacyHome(): React.JSX.Element {
-  return <Redirect to="/v1/introduction" />;
+  return <Redirect to={useBaseUrl('/v1/introduction')} />;
 }
